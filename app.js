718b3bd45562a25bdec7641a9e6f783ce56d3bc3
Mark Sosa <marcososarosales@gmail.com>
2021-04-18 21:01:26 -0500

    api with node, express and mysql in developing stage

---
+++ app.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3050;
+
+const app = express();
+app.use(bodyParser.json());
+
+//mySql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'spidermark',
+    database: 'nodeexmysql'
+});
+
+//Create a route for the endpoint /
+app.get('/', (req, res) => {
+    res.send('Welcome to my API');
+})
+
+//endpoint "movies"
+app.get('/movies', (req, res) => {
+    const sql = 'SELECT * FROM ghibli';
+    
+    connection.query(sql, (error, results) => {
+        if(error) throw error;
+        if(results.length > 0){
+            res.json(results);
+        }else{
+            res.send('No results');
+        }
+    });
+
+    //res.send('List of Ghibli movies');
+})
+
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = `SELECT * FROM ghibli WHERE id = ${id}`;
+    connection.query(sql, (error, results) => {
+        if(error) throw error;
+        if(results.length > 0){
+            res.json(results);
+        }else{
+            res.send('No results');
+        }
+    });
+})
+
+app.post('/add', (req, res) => {
+    
+    const sql = 'INSERT INTO ghibli SET ?';
+    const movieObj = {
+        title: req.body.title,
+        original_title: req.body.original_title,
+        original_title_romanised:  req.body.original_title_romanised,
+        description:  req.body.description,
+        director:  req.body.director,
+        release_date:  req.body.release_date
+    }
+
+    connection.query(sql, movieObj, error => {
+        if(error) throw error;
+        res.send('Movie added');
+    });
+});
+
+app.put('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, 
+            original_title,
+            original_title_romanised,
+            description,
+            director,
+            release_date
+        } = req.body;
+    const sql = `UPDATE ghibli SET title = '${title}' , original_title = '${original_title}', original_title_romanised = '${original_title_romanised}', description = '${description}', director = '${director}', release_date = '${release_date}' WHERE id = ${id}`;
+
+    connection.query(sql, error => {
+        if(error) throw error;
+        res.send('Record updated');
+    });
+});
+
+app.delete('/delete/:id', (req,res) => {
+    const { id } = req.params;
+    const sql = `DELETE FROM ghibli WHERE id = ${id}`;
+    connection.query(sql, (error, results) => {
+        if(error) throw error;
+        res.send('Record deleted');
+    });
+})
+
+//Check connection
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Database server running!')
+})
+
+app.listen(PORT, ()=> console.log(`Server running in port ${PORT}`));

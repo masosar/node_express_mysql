fb6b2a0c95bd8b6a5d93703786e7f26dd8f9f855
Mark Sosa <marcososarosales@gmail.com>
2021-04-20 10:23:58 -0500

    Going back from previous

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "http://marcososa.me");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
-  // next();
+//  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(bodyParser.json());

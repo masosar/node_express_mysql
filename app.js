8dd963381c35690b12e1e456a71d791e20820e07
Mark Sosa <marcososarosales@gmail.com>
2021-04-30 23:29:16 -0500

    Remove authoirization

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
+    "Origin, X-Requested, Content-Type"
   )
   
       // intercept OPTIONS method
